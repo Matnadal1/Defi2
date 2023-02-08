ec89d22e06d535ecccb4be09a2c32ea086aca0d6
Matnadal1 <matnadal.star2@gmail.com>
2023-02-08 15:26:12 +0100

    fin du 2.2 et 2.3

--- scripts/script_ex1.js
+++ scripts/script_ex1.js
@@ -22,6 +22,42 @@ const config = {
 };
 
 const myChart = new Chart(
-    document.querySelector('canvas'),
+    document.querySelector('.chart1'),
     config
 );
+
+const labels2 = [
+    '2018-19',
+    '2019-20',
+    '2020-21',
+    '2021-22',
+];
+
+const data2 = {
+    labels: labels2,
+    datasets: [{
+        label: 'Nombre d\'inscrits homme',
+        backgroundColor: 'rgb(0, 199, 193)',
+        borderColor: 'rgb(0, 199, 193)',
+        data: [37, 37, 85, 90],
+    },{
+        label: 'Nombre d\'inscrits femme',
+        backgroundColor: 'rgb(199, 0, 193)',
+        borderColor: 'rgb(199, 0, 193)',
+        data: [40, 32, 49, 58],
+    }]
+};
+
+
+const config2 = {
+    type: 'line',
+    data: data2,
+    options: {}
+};
+
+const myChart2 = new Chart(
+    document.querySelector('.chart2'),
+    config2
+);
+
+
