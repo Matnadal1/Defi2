7fcc0082f2d2ce83b0768ec385b76029e6e4f412
Matnadal1 <matnadal.star2@gmail.com>
2023-02-08 14:45:23 +0100

    ajout de l'exo 1

---
+++ scripts/script_ex1.js
@@ -0,0 +1,16 @@
+const labels = [
+    '2018-19',
+    '2019-20',
+    '2020-21',
+    '2021-22',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Les inscrits à l’UT2J en L2 MIASHS,
+        backgroundColor: 'rgb(0, 199, 193)',
+        borderColor: 'rgb(0, 199, 193)',
+        data: [79, 70, 134, 149],
+    }]
+};
